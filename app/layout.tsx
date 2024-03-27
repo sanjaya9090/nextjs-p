6cb9668376f678fd7d3dce9a8c6f1fe31a0f859e
Sanjay <sanjaysingh@ibm.com>
2024-03-27 11:21:46 +0530

    test2

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,13 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        {children}
-        <p>
-          चूंकि मानव अधिकारों के प्रति उपेक्षा और घृणा के फलस्वरूप ही ऐसे बर्बर
-          कार्य हुए जिनसे मनुष्य
-        </p>
-      </body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
